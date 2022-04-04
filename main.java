4a3aa00e372cf7846c801724d43f1f2806de81a9
zainabzoaib <zainab.zoaib@gmail.com>
2022-04-05 00:04:08 +1000

    Update main.java
    reading file

--- main.java
+++ main.java
@@ -1,33 +1,49 @@
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 /**
  * Write a description of class main here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class main
+public class Main
 {
     // instance variables - replace the example below with your own
-    String line;
-    Double result;
-    String[] tokens;
+    private String unitName;
+    private ArrayList<String> lastName;
+    private ArrayList<String> firstName;
+    private ArrayList<Integer> studentId;
+    private ArrayList<Double> A1;
+    private ArrayList<Double> A2;
+    private ArrayList<Double> A3;
+    private String row;
+    private String [] heads;
 
     /**
      * Constructor for objects of class main
      */
-    public main()
+    public Main()
     {
         // initialise instance variables
+    int lineNumbers=0;
        try{
             File myFile=new File("student.csv");
             Scanner myScanner=new Scanner(myFile);
             while(myScanner.hasNextLine()){
-                 line=myScanner.nextLine();
-                 tokens = line.split(",");
-                 //System.out.println(tokens[0]);
-                 System.out.println(line);
+                 row=myScanner.nextLine();
+                 if(lineNumbers==0){
+                     heads = row.split(",");
+                     unitName = heads[0];
+                     System.out.println(unitName);
+                    }
+                    else if(lineNumbers>1){
+                    heads = row.split(",");
+                    System.out.printf("%S %S %S %n",heads[0],heads[1],heads[2]);
+                    }
+                    lineNumbers++;
             }
             myScanner.close();
         }catch(FileNotFoundException e){
@@ -43,7 +59,7 @@ public class main
      * @return    the sum of x and y
      */
     public static void main(String[] args){
-        main myObj= new main();
+        Main myObj= new Main();
         
     }
 }
