0fdc6cf179db0f87c61223b299ebe9f8de4a164f
zainabzoaib <zainab.zoaib@gmail.com>
2022-04-05 20:49:53 +1000

    Update main.java
    Sorting data

--- main.java
+++ main.java
@@ -3,6 +3,9 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Write a description of class main here.
  *
@@ -16,9 +19,9 @@ public class Main
     private ArrayList<String> lastName;
     private ArrayList<String> firstName;
     private ArrayList<Integer> studentId;
-    private ArrayList<Double> A1;
-    private ArrayList<Double> A2;
-    private ArrayList<Double> A3;
+    private ArrayList<Double> assignment1;
+    private ArrayList<Double> assignment2;
+    private ArrayList<Double> assignment3;
     private String row;
     private String [] heads;
 
@@ -28,24 +31,50 @@ public class Main
     public Main()
     {
         // initialise instance variables
-    int lineNumbers=0;
-       try{
-            File myFile=new File("student.csv");
-            Scanner myScanner=new Scanner(myFile);
+        int lineNumbers=0;
+        HashMap<String, List<Double>> map = new HashMap<String, List<Double>>();
+        try{
+            File studentFile=new File("student.csv");
+            Scanner myScanner=new Scanner(studentFile);
             while(myScanner.hasNextLine()){
-                 row=myScanner.nextLine();
-                 if(lineNumbers==0){
-                     heads = row.split(",");
-                     unitName = heads[0];
-                     System.out.println(unitName);
-                    }
-                    else if(lineNumbers>1){
+                row=myScanner.nextLine();
+                if(lineNumbers==0){
                     heads = row.split(",");
-                    System.out.printf("%S %S %S %n",heads[0],heads[1],heads[2]);
+                    unitName = heads[0];
+                    System.out.println(unitName);
+                }
+                else if(lineNumbers>1){
+                    heads = row.split(",");
+                    ArrayList<Double> stdMarks= new ArrayList<Double>();
+                    if (3>heads.length-1 || heads[3].equals("")){
+                        stdMarks.add(0.0);
                     }
-                    lineNumbers++;
+                    else{
+                        stdMarks.add(Double.valueOf(heads[3]));
+                    }
+                    if (4>heads.length-1 || heads[3].equals("")){
+                        stdMarks.add(0.0);
+                    }
+                    else{
+                        stdMarks.add(Double.valueOf(heads[4]));
+                    }
+
+                    if (5>heads.length-1 || heads[3].equals("")){
+                        stdMarks.add(0.0);
+                    }
+                    else{
+                        stdMarks.add(Double.valueOf(heads[5]));
+                    }
+
+                    stdMarks.add(Double.valueOf( heads[2]));
+                    map.put(heads[0]+" "+ heads[1], stdMarks);
+
+                }
+                lineNumbers++;
             }
             myScanner.close();
+            System.out.println(map);
+            System.out.println(map.size());
         }catch(FileNotFoundException e){
             System.out.println("The file cannot be found");
             e.printStackTrace();
@@ -60,6 +89,6 @@ public class Main
      */
     public static void main(String[] args){
         Main myObj= new Main();
-        
+
     }
 }
